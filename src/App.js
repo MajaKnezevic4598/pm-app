c13d77d1dba78198c8c056c30b22c087d5854555
filipstev <filipstevanovic176@gmail.com>
2022-03-18 12:39:48 +0100

    dodata struktura

--- src/App.js
+++ src/App.js
@@ -2,10 +2,7 @@ import { useEffect } from 'react';
 import axiosInstance from './helpers/axiosInstance';
 
 function App() {
-  useEffect(() => {
-    axiosInstance.get('/categories').then((data) => console.log(data));
-  }, []);
-  return <div className="App">Hello world!</div>;
+  return <div className="App"></div>;
 }
 
 export default App;
