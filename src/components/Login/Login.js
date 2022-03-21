a13a5e02391c2dc6275283f22afb57beac7a8744
filipstev <filipstevanovic176@gmail.com>
2022-03-21 10:08:24 +0100

    dodat login

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,11 +1,15 @@
-import React, { useEffect, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import './Login.scss';
 import { useNavigate } from 'react-router-dom';
 import useInput from '../../hooks/use-input';
-
+import axiosInstance from '../../helpers/axiosInstance';
+import AuthContext from '../../context/AuthContext';
+import Spinner from '../Spinner/Spinner';
 const Login = () => {
   const navigate = useNavigate();
   const [hasError, setHasError] = useState(false);
+  const { getLoggedIn } = useContext(AuthContext);
+  const [isLoading, setIsLoading] = useState(false);
 
   const emailRegEx =
     /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
@@ -28,12 +32,29 @@ const Login = () => {
     reset: resetPasswordInput,
   } = useInput((value) => +value.length > 5);
 
-  const loginHandler = () => {
+  const loginHandler = async () => {
     if (emailIsValid && passwordIsValid) {
-      console.log(enteredEmail);
-      console.log(enteredPassword);
+      setIsLoading(true);
+      try {
+        const data = await axiosInstance.post('/auth/local', {
+          identifier: enteredEmail,
+          password: enteredPassword,
+        });
+        if (data) {
+          localStorage.setItem('token', data.data.jwt);
+          localStorage.setItem('userId', data.data.user.id);
+
+          getLoggedIn();
+          console.log(data);
+        }
+        setIsLoading(false);
+      } catch (e) {
+        setIsLoading(false);
+        console.log(e);
+      }
     } else {
       setHasError(true);
+      setIsLoading(false);
     }
   };
 
@@ -46,87 +67,99 @@ const Login = () => {
   }, [enteredEmail, enteredPassword]);
 
   return (
-    <div className="login">
-      <div className="login__top">
-        <div className="login__circle"></div>
-        <div className="login__top__text">
-          <div
-            style={{
-              fontSize: '3rem',
-              display: 'flex',
-              flexDirection: 'column',
-            }}
-          >
-            <span>Welcome</span>
-            <span style={{ marginTop: '-10px' }}>Back</span>
+    <>
+      {!isLoading ? (
+        <div className="login">
+          <div className="login__top">
+            <div className="login__circle"></div>
+            <div className="login__top__text">
+              <div
+                style={{
+                  fontSize: '3rem',
+                  display: 'flex',
+                  flexDirection: 'column',
+                }}
+              >
+                <span>Welcome</span>
+                <span style={{ marginTop: '-10px' }}>Back</span>
+              </div>
+              <div style={{ marginTop: '10px' }}>
+                Please sign-in to continue!
+              </div>
+            </div>
           </div>
-          <div style={{ marginTop: '10px' }}>Please sign-in to continue!</div>
-        </div>
-      </div>
-      <form
-        className="login__bottom"
-        onSubmit={(e) => {
-          e.preventDefault();
-          loginHandler();
-        }}
-      >
-        <input
-          value={enteredEmail}
-          onChange={emailChangedHandler}
-          type="email"
-          placeholder="Email"
-          autoComplete="email"
-          style={{
-            border:
-              !emailIsValid && hasError ? '1px solid red' : '1px solid #eee',
-          }}
-        />
-        <input
-          value={enteredPassword}
-          onChange={passwordChangedHandler}
-          type={'password'}
-          placeholder="Password"
-          autoComplete="current-password"
-          style={{
-            border:
-              !passwordIsValid && hasError ? '1px solid red' : '1px solid #eee',
-          }}
-        />
-        {hasError ? (
-          <div
-            style={{
-              color: 'red',
+          <form
+            className="login__bottom"
+            onSubmit={(e) => {
+              e.preventDefault();
+              loginHandler();
             }}
           >
-            {hasError && enteredPassword.length < 6
-              ? 'Password too short'
-              : 'Invalid input'}
-          </div>
-        ) : null}
-        <div className="login__bottom__forgot">Forgot your password?</div>
-        <button
-          type="submit"
-          className="login__bottom__button"
-          onClick={loginHandler}
-        >
-          Signin
-        </button>
-        <div className="login__bottom_signup">
-          Don't have an account?{' '}
-          <span
-            style={{
-              color: '#fbb440',
-              fontWeight: 'bold',
-              marginLeft: '5px',
-              cursor: 'pointer',
-            }}
-            onClick={() => navigate('/register')}
-          >
-            Signup
-          </span>
+            <input
+              value={enteredEmail}
+              onChange={emailChangedHandler}
+              type="email"
+              placeholder="Email"
+              autoComplete="email"
+              style={{
+                border:
+                  !emailIsValid && hasError
+                    ? '1px solid red'
+                    : '1px solid #eee',
+              }}
+            />
+            <input
+              value={enteredPassword}
+              onChange={passwordChangedHandler}
+              type={'password'}
+              placeholder="Password"
+              autoComplete="current-password"
+              style={{
+                border:
+                  !passwordIsValid && hasError
+                    ? '1px solid red'
+                    : '1px solid #eee',
+              }}
+            />
+            {hasError ? (
+              <div
+                style={{
+                  color: 'red',
+                }}
+              >
+                {hasError && enteredPassword.length < 6
+                  ? 'Password too short'
+                  : 'Invalid input'}
+              </div>
+            ) : null}
+            <div className="login__bottom__forgot">Forgot your password?</div>
+            <button
+              type="submit"
+              className="login__bottom__button"
+              onClick={loginHandler}
+            >
+              Signin
+            </button>
+            <div className="login__bottom_signup">
+              Don't have an account?{' '}
+              <span
+                style={{
+                  color: '#fbb440',
+                  fontWeight: 'bold',
+                  marginLeft: '5px',
+                  cursor: 'pointer',
+                }}
+                onClick={() => navigate('/register')}
+              >
+                Signup
+              </span>
+            </div>
+          </form>
         </div>
-      </form>
-    </div>
+      ) : (
+        <Spinner />
+      )}
+    </>
   );
 };
 
