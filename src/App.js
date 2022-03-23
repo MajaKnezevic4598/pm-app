43cee7fcdbb8b148ab6c4d0ed210aa7f72808b8c
RobertBognar <79517001+RobertBognar@users.noreply.github.com>
2022-03-23 09:52:58 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,29 +3,19 @@ import Login from './components/Login/Login';
 import AuthContext from './context/AuthContext';
 import axiosInstance from './helpers/axiosInstance';
 import AdminRoutes from './routes/AdminRoutes';
-import EmployeeRoutes from './routes/EmployeeRoutes';
-import PMRoutes from './routes/PMRoutes';
 import PublicRoutes from './routes/PublicRoutes';
 
 function App() {
-  //vercel
-  const { loggedIn } = useContext(AuthContext);
-
-  const userRole = localStorage.getItem('role');
-
-  return (
-    <div className="App">
-      {loggedIn && userRole === 'system_administrator' ? (
-        <AdminRoutes />
-      ) : loggedIn && userRole === 'employee' ? (
-        <EmployeeRoutes />
-      ) : loggedIn && userRole === 'project_manager' ? (
-        <PMRoutes />
-      ) : (
-        <PublicRoutes />
-      )}
-    </div>
-  );
+    //vercel
+    const { loggedIn } = useContext(AuthContext);
+    useEffect(() => {
+        console.log(loggedIn);
+    }, [loggedIn]);
+    return (
+        <div className="App">
+            {loggedIn ? <AdminRoutes /> : <PublicRoutes />}
+        </div>
+    );
 }
 
 export default App;
