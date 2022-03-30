59c08ed57c241354ca00a48e3074439e76f4842e
filipstev <filipstevanovic176@gmail.com>
2022-03-30 16:01:46 +0200

    obrisan header iz login/register

--- src/routes/PublicRoutes.js
+++ src/routes/PublicRoutes.js
@@ -6,19 +6,19 @@ import Login from '../components/Login/Login';
 import Register from '../components/Register/Register';
 
 const UserRoutes = (props) => {
-    return (
-        <>
-            <BrowserRouter>
-                {/* Move Header To Admin Routes On Next Push */}
-                <Header />
-                <Routes>
-                    <Route path="/" element={<Login />} />
-                    <Route path="/register" element={<Register />} />
-                    <Route path="*" element={<Navigate replace to="/" />} />
-                </Routes>
-            </BrowserRouter>
-        </>
-    );
+  return (
+    <>
+      <BrowserRouter>
+        {/* Move Header To Admin Routes On Next Push */}
+        {/* <Header /> */}
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="*" element={<Navigate replace to="/" />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 };
 
 export default UserRoutes;
