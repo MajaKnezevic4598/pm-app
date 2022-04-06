e01f8c355ff40ad85b8def432ea94530c8842a35
RobertBognar <79517001+RobertBognar@users.noreply.github.com>
2022-04-06 10:48:40 +0200

    Update SingleProjectCard.js

--- src/components/PMHome/SingleProjectCard.js
+++ src/components/PMHome/SingleProjectCard.js
@@ -11,82 +11,33 @@ import { useNavigate } from 'react-router-dom';
 Modal.setAppElement('#root');
 
 const SingleProjectCard = ({
-  manager,
-  prName,
-  pmImage,
-  projectLogo,
-  employees,
-  projectDescription,
-  projectId,
-  id,
+    manager,
+    prName,
+    pmImage,
+    projectLogo,
+    employees,
+    projectDescription,
+    projectId,
+    id,
 }) => {
-  const [modalIsOpen, setModalIsOpen] = useState(false);
-  return (
-    <div className="card" id={`${projectId}`} onClick={() => navigate(`/${id}/notes`)}>
-      <div className="card__img-conteiner">
-        {projectLogo ? (
-          <img
-            className="card__project-logo"
-            src={`https://pm-app-bek.herokuapp.com${projectLogo}`}
-            alt="projectLogo"
-          />
-        ) : (
-          <img src={img} className="card__project-logo" alt="projectLogo" />
-        )}
-      </div>
-      <div className="card__info-1">
-        <h2>{prName}</h2>
-        <div>
-          {pmImage ? (
-            <img
-              src={`https://pm-app-bek.herokuapp.com${pmImage}`}
-              alt="neka"
-              className="project-manager-image"
-            />
-          ) : (
-            <img src={img} alt="default" />
-          )}
-
-          <span>{manager}</span>
-        </div>
-      </div>
-      <div className="card__info-2">
-        <MdOpenInNew
-          className="card__info-2__icon"
-          onClick={() => {
-            setModalIsOpen(true);
-            window.localStorage.setItem("projectId", projectId);
-          }}
-        />
-        <p>{employees} employees</p>
-      </div>
-      <Modal
-        isOpen={modalIsOpen}
-        onRequestClose={() => {
-          setModalIsOpen(false);
-        }}
-        className="Modal"
-        overlayClassName="Overlay"
-      >
-        <div className="Modal__conteiner">
-          <section className="modal-main-section">
-            <div>
-              {/* <img src={img} alt="" className="modal-main-section__img" /> */}
-
-              {projectLogo ? (
-                <img
-                  className="modal-main-section__img"
-                  src={`https://pm-app-bek.herokuapp.com${projectLogo}`}
-                  alt="projectLogo"
-                />
-              ) : (
-                <img
-                  src={img}
-                  className="modal-main-section__img"
-                  alt="projectLogo"
-                />
-              )}
-
+    const navigate = useNavigate();
+    const [modalIsOpen, setModalIsOpen] = useState(false);
+    return (
+        <div className="card" id={`${projectId}`} onClick={() => navigate(`/${id}/notes`)}>
+            <div className="card__img-conteiner">
+                {projectLogo ? (
+                    <img
+                        className="card__project-logo"
+                        src={`https://pm-app-bek.herokuapp.com${projectLogo}`}
+                        alt="projectLogo"
+                    />
+                ) : (
+                    <img
+                        src={img}
+                        className="card__project-logo"
+                        alt="projectLogo"
+                    />
+                )}
             </div>
             <div className="card__info-1">
                 <h2>{prName}</h2>
