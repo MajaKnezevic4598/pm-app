52f151f735b96dc2f14e9378ea669eea45460336
Maja Knezevic <maja.knezevic1@gmail.com>
2022-04-20 15:27:07 +0200

    styles added

--- src/components/EmployeeHome/EmptyNote.js
+++ src/components/EmployeeHome/EmptyNote.js
@@ -1,18 +1,14 @@
-import React from 'react';
+import React from "react";
 
-import './EmptyNote.scss';
+import "./EmptyNote.scss";
 
 const EmptyNote = () => {
-    return (
-        <div className="empty">
-            <div className="empty__text">
-                <div className="empty__text__title">
-                    There are no information at the moment.
-                </div>
-                <div>Please check back later!</div>
-            </div>
-        </div>
-    );
+  return (
+    <div className="empty">
+      <p>There are no information at the moment.</p>
+      <p>Please check back later!</p>
+    </div>
+  );
 };
 
 export default EmptyNote;
